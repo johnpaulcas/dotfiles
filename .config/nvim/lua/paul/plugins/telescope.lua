f006a8f9a81954e7d4877ddce0200b31c86cfaef
johnpaulcas <casjp.app@gmail.com>
2025-01-13 08:00:58 +0800

    refactor: change fs to fg

--- .config/nvim/lua/paul/plugins/telescope.lua
+++ .config/nvim/lua/paul/plugins/telescope.lua
@@ -10,17 +10,17 @@ return {
   config = function()
     local telescope = require("telescope")
     local actions = require("telescope.actions")
-  --  local transform_mod = require("telescope.actions.mt").transform_mod
+    --  local transform_mod = require("telescope.actions.mt").transform_mod
 
-   -- local trouble = require("trouble")
-   -- local trouble_telescope = require("trouble.sources.telescope")
+    -- local trouble = require("trouble")
+    -- local trouble_telescope = require("trouble.sources.telescope")
 
     -- or create your custom action
-   --local custom_actions = transform_mod({
-   --  open_trouble_qflist = function(prompt_bufnr)
-   --    trouble.toggle("quickfix")
-   --  end,
-   --})
+    --local custom_actions = transform_mod({
+    --  open_trouble_qflist = function(prompt_bufnr)
+    --    trouble.toggle("quickfix")
+    --  end,
+    --})
 
     telescope.setup({
       defaults = {
@@ -42,8 +42,8 @@ return {
 
     keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
     keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
-    keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
+    keymap.set("n", "<leader>fg", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
     keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
- keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<cr>", { desc = "Find todos" })
+    keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<cr>", { desc = "Find todos" })
   end,
 }
