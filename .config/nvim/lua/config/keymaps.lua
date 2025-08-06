b1694f9ee7a8be650688c2969a8e68aaaf83dc28
John Paul Cas <johnpaulcas@Johns-MacBook-Pro.local>
2025-08-07 06:22:28 +0800

    ✨auto center

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -8,6 +8,22 @@ vim.keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights
 vim.keymap.set("n", "<Esc>", ":nohlsearch<CR>")
 vim.keymap.set("n", "<leader>h", ":nohlsearch<CR>")
 
+-- Vertical scroll and center
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+-- Verical block search and center
+vim.keymap.set("n", "{", "{zz")
+vim.keymap.set("n", "}", "}zz")
+
+-- Center screen when searching forward and backward
+vim.keymap.set("n", "n", "nzz")
+vim.keymap.set("n", "N", "Nzz")
+
+-- Center screen when searching for word under cursor
+vim.keymap.set("n", "*", "*zz")
+vim.keymap.set("n", "#", "#zz")
+
 -- formatter --
 vim.keymap.set("n", "<leader>cf", function()
 	require("conform").format({ lsp_format = "fallback" })
