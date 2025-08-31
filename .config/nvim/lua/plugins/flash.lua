f5eb1148de392567182b7460c4eec98af7268d31
John Paul Cas <paul@Johns-MacBook-Pro.local>
2025-08-31 16:42:59 +0800

    🎨 enable jump labels

--- .config/nvim/lua/plugins/flash.lua
+++ .config/nvim/lua/plugins/flash.lua
@@ -2,7 +2,14 @@ return {
 	"folke/flash.nvim",
 	event = "VeryLazy",
 	---@type Flash.Config
-	opts = {},
+	opts = {
+		modes = {
+			enabled = true,
+		},
+		char = {
+			jump_labels = true,
+		},
+	},
     -- stylua: ignore
     keys = {
         { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
