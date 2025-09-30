9dcf614e37b712908c913be5282a1c44c14a8f0d
johnpaulcas <john.c@sovrun.org>
2025-09-30 13:04:59 +0800

    ✨ add gofmt formatter

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -15,6 +15,7 @@ return {
 				css = { "prettierd", "prettier", stop_after_first = true },
 				scss = { "prettierd", "prettier", stop_after_first = true },
 				html = { "prettierd", "prettier", stop_after_first = true },
+				go = { "goimports", "gofmt", stop_after_first = true },
 			},
 			format_on_save = {
 				-- These options will be passed to conform.format()
